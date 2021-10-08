f0cf2e00ea29caef6d0f7cf229fea947fdc97b46
francescoFH <francescofacca@gmail.com>
2021-10-08 12:41:57 +0100

    Define find_country as private method and also define a private and generic method to fetch data from APIs

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -10,4 +10,24 @@ class TravelController < ApplicationController
       return render action: :index
     end
   end
+
+  private
+
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-RapidAPI-Host' => URI.parse(url).host,
+        'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+      }
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+  
+  def find_country(name)
+    request_api(
+      "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+    )
+  end
 end
